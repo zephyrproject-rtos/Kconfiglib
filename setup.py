83c9aeb6ffa498681b9c71c7186bd8956acc38bf
Stephanos Ioannidis <root@stephanos.io>
2024-10-04 12:33:52 +0900

    setup.py: Add Python 3.9-3.12 classifiers
    This commit adds the classifiers for the Python versions from 3.9 to
    3.12 since these versions have been tested to be compatible.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- setup.py
+++ setup.py
@@ -90,6 +90,10 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ]
